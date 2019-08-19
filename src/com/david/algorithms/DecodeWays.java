d909f4ecaa6ad00e4aa06ff8909210af885d5c82
David <dhernandez@intersysconsulting.com>
2019-08-19 17:40:23 -0500

    Add Rotate Matrix

--- src/com/david/algorithms/DecodeWays.java
+++ src/com/david/algorithms/DecodeWays.java
@@ -8,9 +8,33 @@ public class DecodeWays {
     System.out.println("226: 3=" + dw.numDecodings("226"));
     System.out.println("110: 1=" + dw.numDecodings("110"));
     System.out.println("1010: 1=" + dw.numDecodings("1010"));
+    System.out.println("12: 2=" + dw.numDecodings("12"));
+  }
+  public int numDecodings(String A) {
+    int size = A.length();
+    if(A.charAt(0) == '0'){
+      return 0;
+    }
+    int[] count = new int[size + 1];
+    count[0] = 1;
+    count[1] = 1;
+
+    for(int i = 2; i<= size; i++) {
+      count[i] = 0;
+
+      if(A.charAt(i-1) != '0') {
+        count[i] = count[i-1];
+      }
+
+      int number = (count[i-2] - '0') * 10 + (count[i-1] - '0');
+      if(number > 9 && number < 27) {
+        count[i] += count[i-2];
+      }
+    }
+    return count[size];
   }
 
-  public int numDecodings(String A) {
+  public int numDecodings2(String A) {
     int size = A.length();
 
     // validate that the first digit is not 0
